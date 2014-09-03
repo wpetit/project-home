c4b8a01cc612c7dfc5d0e68cc945f58f9d993f61
wpetit <petit.wilfried@gmail.com>
2014-09-03 13:58:51 +0200

    Mise à jour de la license + robustesse sur la récupération de l'URL des jobs + ajout des images github et nexus

--- WebContent/scripts/service.js
+++ WebContent/scripts/service.js
@@ -3,15 +3,15 @@ app.service('environmentService', function($http) {
 		return $http.get('resources/environment.json');
 	};
 	this.getJobInformation = function(jenkinsUrl,job) {
-		return $http.get(jenkinsUrl+'job/'+job+'/api/json');
+		return $http.get(jenkinsUrl+'/job/'+job+'/api/json');
 	};
 	this.getBuildDetail = function(jenkinsUrl,build) {
-		return $http.get(build.url.substring(build.url.indexOf('/jenkins')).replace('/jenkins',jenkinsUrl)+'/api/json'); 
+		return $http.get(build.url.substring(build.url.indexOf('/job')).replace('/job',jenkinsUrl+'/job')+'/api/json'); 
 	}
 	this.getSonarResourcesAndAnalysisPeriod = function(sonarUrl) {
-		return $http.get(sonarUrl+'api/resources?includetrends=true'); 
+		return $http.get(sonarUrl+'/api/resources?includetrends=true'); 
 	}
 	this.getSonarViolationsByResourceAndTime = function(sonarUrl,resourceKey,fromDate) {
-		return $http.get(sonarUrl+'api/timemachine?resource='+resourceKey+'&fromDateTime='+fromDate+'&metrics=violations,blocker_violations,critical_violations,major_violations,minor_violations,info_violations,violations_density'); 
+		return $http.get(sonarUrl+'/api/timemachine?resource='+resourceKey+'&fromDateTime='+fromDate+'&metrics=violations,blocker_violations,critical_violations,major_violations,minor_violations,info_violations,violations_density'); 
 	}
 });
